7cdf2fbc131cd4c7e3b5b0f2418b29db04a0bca1
Nick Zhao <nickzrz@hotmail.com>
2018-09-16 14:31:20 -0700

    Almost Finished. Need to finsih pop

--- Heap_Tree/Heap_Tree/Heap_Tree.cs
+++ Heap_Tree/Heap_Tree/Heap_Tree.cs
@@ -15,32 +15,104 @@ namespace Heap_Tree
             Count = 0;
         }
 
+        private int Left(int index)
+        {
+            return (2 * index) + 1;
+        }
+
+        private int Right(int index)
+        {
+            return (2 * index) + 2;
+        }
+
+        private int Parent(int index)
+        {
+            return (index - 1) / 2;
+        }
+
         public void Insert(T value)
         {
-            if (Count + 1 == HTree.Length)
+            if (Count == HTree.Length)
             {
-                //increase array
-                //make a private function for it
+                ChangeSize(HTree.Length * 2);
             }
 
             HTree[Count] = value;
+            HeapifyUp(Count);
             Count++;
-            throw new NotImplementedException();
         }
 
-        public void HeapifyUp()
+        public void HeapifyUp(int index)
         {
-            throw new NotImplementedException();
+            //escape case
+            if (index == 0)
+            {
+                return;
+            }
+
+            int parentIndex = Parent(index);
+            if (HTree[index].CompareTo(HTree[parentIndex]) < 0)
+            {
+                Swap(index, parentIndex);
+                //swap with parent and recurse
+                HeapifyUp(parentIndex);
+            }
         }
 
-        public void Pop()
+        public T Pop()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The Tree is Empty");
+            }
+
+            if (Count == HTree.Length / 4)
+            {
+                ChangeSize(HTree.Length / 2);
+            }
+
+            T ret = HTree[0];
+            //remove the root
+            //place the last item into index 0
+            Count--;
+            HeapifyDown(0);
+            return ret;
         }
 
-        public void HeapifyDown()
+        public void HeapifyDown(int index)
         {
-            throw new NotImplementedException();
+            int left = Left(index);
+            int right = Right(index);
+
+            if (left < Count && HTree[index].CompareTo(HTree[left]) <= 0)
+            {
+                Swap(index, left);
+                HeapifyDown(left);
+            }
+
+            if (right < Count && HTree[parent].CompareTo(HTree[right]) <= 0)
+            {
+                parent = right;
+                Swap(index, parent);
+                HeapifyDown(parent);
+            }
+        }
+
+        private void ChangeSize(int size)
+        {
+            T[] ts = new T[size];
+            for (int i = 0; i < HTree.Length; i++)
+            {
+                ts[i] = HTree[i];
+            }
+            HTree = ts;
+        }
+
+        private void Swap(int i, int k)
+        {
+            T temp = HTree[i];
+            HTree[i] = HTree[k];
+            HTree[k] = temp;
         }
     }
 }
--- Heap_Tree/Heap_Tree/Program.cs
+++ Heap_Tree/Heap_Tree/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Text;
 namespace Heap_Tree
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+            Heap_Tree<int> heap = new Heap_Tree<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                heap.Insert(rand.Next(1, 100));
+            }
+
+            while (heap.Count > 0)
+            {
+                Console.Write($"{heap.Pop()} ");
+            }
+            Console.ReadKey();
         }
     }
 }
